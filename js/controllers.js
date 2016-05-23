648410e2c11440b4a680f615ab5021cfb8130f53
xa17d <daniel.git@xa1.at>
2016-05-23 22:06:09 +0200

    init bootstrap and angular

---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+var app = angular.module('dijkstraApp', ['ngAnimate', 'ui.bootstrap']);
+
+app.controller('MainController', ['$scope', function ($scope) {
+
+    $scope.name = "World";
+
+}]);
