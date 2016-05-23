7c46730af4602c1f8493b871fb127ff4d77db794
xa17d <daniel.git@xa1.at>
2016-05-23 22:49:14 +0200

    Graph and Algorithmus model (mock).
    Highlight in Source-Code

--- js/algorithmus.js
+++ js/algorithmus.js
@@ -1,1 +1,54 @@
-﻿
+﻿'use strict';
+
+function Vertex(name) {
+    // Vertex properties
+    this.name = name;
+    this.neighbors = [];
+
+    // State in algorithmus
+    this.previous = null;
+    this.distance = Number.POSITIVE_INFINITY;
+}
+
+function Graph() {
+    this.vertices = [];
+    this.addVertices = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.vertices.push(arguments[i]);
+        }
+    }
+
+    this.addEdge = function (vertex1, vertex2, distance) {
+        vertex1.neighbors.push(
+            {
+                vertex: vertex2,
+                distance: distance
+            }
+        );
+
+        vertex2.neighbors.push(
+            {
+                vertex: vertex1,
+                distance: distance
+            }
+        );
+    }
+}
+
+function DijkstraAlgorithmus(graph, start) {
+    this.status = 'start';
+
+    this.next = function () {
+        // TODO: implement real stuff
+        if (this.status == 'start') { this.status = 'init'; }
+        else if (this.status == 'init') { this.status = 'while'; }
+        else if (this.status == 'while') { this.status = 'getmin'; }
+        else if (this.status == 'getmin') { this.status = 'remove'; }
+        else if (this.status == 'remove') { this.status = 'forneighbor'; }
+        else if (this.status == 'forneighbor') { this.status = 'calcalt'; }
+        else if (this.status == 'calcalt') { this.status = 'comparedist'; }
+        else if (this.status == 'comparedist') { this.status = 'assigndist'; }
+        else if (this.status == 'assigndist') { this.status = 'return'; }
+        else if (this.status == 'return') { this.status = 'start'; }
+    }
+}
--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,37 @@ var app = angular.module('dijkstraApp', ['ngAnimate', 'ui.bootstrap']);
 
 app.controller('MainController', ['$scope', function ($scope) {
 
+    /* Create Example Graph
+
+             2
+        A ------- B
+        |         |
+        | 2       | 4
+        |         |
+        C ------- D-------E
+            1         3
+    */
+
+    var graph = new Graph();
+
+    var a = new Vertex("A");
+    var b = new Vertex("B");
+    var c = new Vertex("C");
+    var d = new Vertex("D");
+    var e = new Vertex("E");
+
+    graph.addVertices(a,b,c,d,e);
+    graph.addEdge(a, b, 2);
+    graph.addEdge(a, c, 2);
+    graph.addEdge(b, d, 4);
+    graph.addEdge(c, d, 1);
+    graph.addEdge(d, e, 3);
+
+    var algorithmus = new DijkstraAlgorithmus(graph, a);
+
+    $scope.algorithmus = algorithmus;
+
     $scope.name = "World";
+    $scope.line = 10;
 
 }]);
